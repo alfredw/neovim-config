1f3f43e7ec569e5e4634867fc5a9d757e0c395b4
Alfred Wong <alfiewong@gmail.com>
2022-04-05 14:13:41 -0400

    Fixed syntax error with then

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -25,7 +25,7 @@ vim.cmd [[
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
-if not status_ok the
+if not status_ok then
   return
 end
 
